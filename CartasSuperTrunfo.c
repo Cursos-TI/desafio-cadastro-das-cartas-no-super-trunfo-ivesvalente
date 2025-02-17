0c6b3be72fef29c3821f11f1f4cabbe0a0a0e9d6
ivesvalente <ivesvalent@gmail.com>
2025-02-17 22:05:27 +0000

    algumas correções

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
-#include <string.h>
 
+//Etrutura para representar uma cidade
 int main() {
     printf("Desafio Super Trunfo\n");
 
-    int população;
-    float área;
+    int populaçao;
+    float area;
     float PIB;
-    int pontos_turísticos;
+    int pontos_turisticos;
     char nome[50];
     char codigo[40];
 
+    //Função para cadastrar uma carta
     printf("Digite o código da cidade (ex: A01): ");
     scanf("%s", codigo);
 
@@ -18,24 +19,25 @@ int main() {
     scanf("%s", nome);
 
     printf("Digite a população: ");
-    scanf("%d", &população);
+    scanf("%d", &populaçao);
 
     printf("Digite a área (em km²): ");
-    scanf("%f", &área);
+    scanf("%f", &area);
 
     printf("Digite o PIB (em bilhões): ");
     scanf("%f", &PIB);
 
     printf("Digite o número de pontos turísticos: ");
-    scanf("%d", &pontos_turísticos);
+    scanf("%d", &pontos_turisticos);
 
+    //Função para exibir os dados de uma carta
     printf("\n--- Dados da Cidade ---\n");
     printf("Código: %s\n", codigo);
     printf("Nome: %s\n", nome);
-    printf("População: %d\n", população);
-    printf("Área: %.2f km²\n", área);
+    printf("População: %d\n", populaçao);
+    printf("Área: %.2f km²\n", area);
     printf("PIB: %.2f bilhões\n", PIB);
-    printf("Pontos Turísticos: %d\n", pontos_turísticos);
+    printf("Pontos Turísticos: %d\n", pontos_turisticos);
 
     return 0;
 }
